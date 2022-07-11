8f6f5177e0a4b26dab907957a01aff3dffbb83da
Vazaiosdev <vazaiosdev@gmail.com>
2022-07-11 21:28:15 +0300

    Initial commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,53 +22,6 @@
 </head>
 <body style="background-color: #f3f3f3">
     <div id="app">
-        <nav style="background-color: #333!important" class="bg-white shadow-md navbar navbar-expand-md navbar-light">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="true" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Right Side Of Navbar -->
-                    <ul class="ml-auto navbar-nav">
-                        <!-- Authentication Links -->
-                        @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
-
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link text-white" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                        <li class="nav-item font-weight-bold mt-3 mt-md-0 ml-0 ml-md-3">
-                            <a class="nav-link text-white" href="/book">ΚΛΕΙΣΕ ΠΡΟΠΟΝΗΣΗ</a>
-                        </li>
-                        <li class="nav-item font-weight-bold mt-3 mt-md-0 ml-0 ml-md-3">
-                            <a class="nav-link text-white" href="/profile">ΠΡΟΦΙΛ</a>
-                        </li>
-                        <!-- <li class="nav-item font-weight-bold mt-3 mt-md-0 ml-0 ml-md-3">
-                            <a class="nav-link text-white" href="/privacy-policy">ΟΡΟΙ & ΠΡΟΥΠΟΘΕΣΕΙΣ</a>
-                        </li> -->
-                        @endguest
-                        @auth 
-                            <li class="nav-item ml-0 ml-md-3 mt-3 mt-md-0">
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST">
-                                    @csrf
-                                    <button class="btn btn-outline-light">
-                                        Logout
-                                    </button>
-                                </form>
-                            </li>                    
-                        @endauth
-                    </ul>
-                </div>                
-            </div>
-        </nav>
 
         <main class="py-4">
             @yield('content')
