f77dd405c1f8239bedf62986fcd95e201e1abb79
Vazaiosdev <vazaiosdev@gmail.com>
2022-07-11 22:00:57 +0300

    Initial commit

--- resources/views/backoffice/orders/index.blade.php
+++ resources/views/backoffice/orders/index.blade.php
@@ -21,7 +21,7 @@ $('#dataTable').DataTable({
     "ordering" : true,
     "scrollCollapse" : true,
     "searching" : true,
-    "columnDefs" : [{"targets":[3], "type":"date-eu"}],
+    "columnDefs" : [{"targets":[4], "type":"date-eu"}],
     "bInfo": true
 });
 </script>
